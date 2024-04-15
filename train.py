988456709e7847ec893fedb4fa16658c4f77254c
wut19 <845403627@qq.com>
2024-04-15 22:33:27 +0800

    update train

--- train.py
+++ train.py
@@ -92,6 +92,10 @@ def parse_args():
 def set_seed(seed):
     np.random.seed(seed)
     torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+    
 
 def main(args):
     """ Load config and set up for training experiment """
